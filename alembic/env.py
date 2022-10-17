d59e290dd446db9bb4a1d1a3936a319fccd223d1
dawmro_ubu <dawidmrotek@protonmail.ch>
2022-10-16 17:22:26 -0700

    add alembic for postgres

--- alembic/env.py
+++ alembic/env.py
@@ -5,15 +5,17 @@ from sqlalchemy import pool
 
 from alembic import context
 from app.models import Base
+from app.config import settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option(
+    "sqlalchemy.url", f'postgresql+psycopg2://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}')
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
@@ -21,14 +23,13 @@ if config.config_file_name is not None:
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
 
-
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
 
-def run_migrations_offline() -> None:
+def run_migrations_offline():
     """Run migrations in 'offline' mode.
 
     This configures the context with just a URL
@@ -52,7 +53,7 @@ def run_migrations_offline() -> None:
         context.run_migrations()
 
 
-def run_migrations_online() -> None:
+def run_migrations_online():
     """Run migrations in 'online' mode.
 
     In this scenario we need to create an Engine
--- alembic/versions/8c82b1632f52_add_user_table.py
+++ alembic/versions/8c82b1632f52_add_user_table.py
@@ -1,8 +1,8 @@
 """add user table
 
-Revision ID: 25e6c28e11c6
-Revises: 404df544a806
-Create Date: 2022-10-09 08:56:58.546775
+Revision ID: 8c82b1632f52
+Revises: 01b2584928a5
+Create Date: 2021-08-29 23:04:56.038967
 
 """
 from alembic import op
@@ -10,25 +10,25 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '25e6c28e11c6'
-down_revision = '404df544a806'
+revision = '8c82b1632f52'
+down_revision = '01b2584928a5'
 branch_labels = None
 depends_on = None
 
 
-def upgrade() -> None:
+def upgrade():
     op.create_table('users',
                     sa.Column('id', sa.Integer(), nullable=False),
                     sa.Column('email', sa.String(), nullable=False),
                     sa.Column('password', sa.String(), nullable=False),
-                    sa.Column('created_at', sa.TIMESTAMP(timezone=True), 
-                              server_default = sa.text('current_timestamp'), nullable=False),
+                    sa.Column('created_at', sa.TIMESTAMP(timezone=True),
+                              server_default=sa.text('now()'), nullable=False),
                     sa.PrimaryKeyConstraint('id'),
                     sa.UniqueConstraint('email')
                     )
     pass
 
 
-def downgrade() -> None:
+def downgrade():
     op.drop_table('users')
     pass
