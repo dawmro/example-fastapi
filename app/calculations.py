e6a75fed390019eae756c00a06674f7e8d6f0935
dawmro <dawidmrotek@protonmail.ch>
2022-10-25 16:51:47 -0700

    more dummy tests

--- app/calculations.py
+++ app/calculations.py
@@ -1,2 +1,36 @@
-def add(num1: int, num2: 2):
+def add(num1: int, num2: int):
     return num1 + num2
+
+
+def subtract(num1: int, num2: int):
+    return num1 - num2
+
+
+def multiply(num1: int, num2: int):
+    return num1 * num2
+
+
+def divide(num1: int, num2: int):
+    return num1 / num2
+
+
+class InsufficientFunds(Exception):
+    pass
+
+def test_bank_set_default_amount(zero_bank_account):
+    assert zero_bank_account.balance == 0
+
+class BankAccount():
+    def __init__(self, starting_balance = 0):
+        self.balance = starting_balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if self.balance < amount:
+            raise InsufficientFunds("Insufficeint balance!")
+        self.balance -= amount
+    
+    def collect_interest(self):
+        self.balance *= 1.1
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,9 +1,77 @@
-from app.calculations import add
-
-def test_add():
-    print("testing add function")
-    sum = add(5, 3)
-    assert sum == 8
+import pytest
+from app.calculations import add, subtract, multiply, divide, BankAccount, InsufficientFunds
 
 
-test_add()
+@pytest.fixture
+def zero_bank_account():
+    return BankAccount()
+
+
+@pytest.fixture
+def bank_account():
+    return BankAccount(50)
+
+
+@pytest.mark.parametrize("num1, num2, expected", [
+    (3, 2, 5),
+    (5, 4, 9),
+    (1, 6, 7)
+])
+
+def test_add(num1, num2, expected):
+    assert add(num1, num2) == expected
+
+
+def test_subtract():
+    assert subtract(5, 3) == 2
+
+
+def test_multiply():
+    assert multiply(5, 3) == 15
+
+
+def test_divide():
+    assert divide(6, 3) == 2
+
+
+
+def test_bank_set_initial_amount():
+    bank_account = BankAccount(50)
+    assert bank_account.balance == 50
+
+
+def test_bank_set_default_amount(zero_bank_account):
+    assert zero_bank_account.balance == 0
+
+
+def test_withdraw(bank_account):
+    bank_account.withdraw(30)
+    assert bank_account.balance == 20
+
+
+def test_deposit(bank_account):
+    bank_account.deposit(30)
+    assert bank_account.balance == 80
+
+
+def test_collect_interest(bank_account):
+    bank_account.collect_interest()
+    assert round(bank_account.balance, 6) == 55
+
+
+
+@pytest.mark.parametrize("deposited, withdrew, expected", [
+    (100, 70, 30),
+    (100, 100, 0),
+    (0, 0, 0)
+])
+
+def test_bank_transaction(zero_bank_account, deposited, withdrew, expected):
+    zero_bank_account.deposit(deposited)
+    zero_bank_account.withdraw(withdrew)
+    assert zero_bank_account.balance == expected
+
+
+def test_insufficient_funds(bank_account):
+    with pytest.raises(InsufficientFunds):
+        bank_account.withdraw(100)
