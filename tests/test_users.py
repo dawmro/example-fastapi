b0381304b83b1bfd03e2393085cc9e449e83d9cc
dawmro <dawidmrotek@protonmail.ch>
2022-10-28 16:32:39 -0700

    test create user

---
+++ tests/test_users.py
@@ -0,0 +1,16 @@
+from app import schemas
+from .database import client, session
+
+
+def test_root(client):
+    res = client.get("/")
+    assert res.json().get("message") == "Hello World"
+    assert res.status_code == 200
+
+
+def test_create_user(client):
+    res = client.post("/users/", json={"email": "hello123@gmial.com", "password": "password123"})
+    
+    new_user = schemas.UserOut(**res.json())
+    assert new_user.email == "hello123@gmial.com"
+    assert res.status_code == 201
