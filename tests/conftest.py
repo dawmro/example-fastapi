974d40a9bcbe7684796cd6d0ed335d24b9c2a93d
dawmro <dawidmrotek@protonmail.ch>
2022-10-31 23:17:47 -0700

    test get create post

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,6 +7,8 @@ from app.main import app
 
 from app.config import settings
 from app.database import get_db, Base
+from app.oauth2 import create_access_token
+from app import models
 
 
 
@@ -50,3 +52,50 @@ def test_user(client):
     new_user = res.json()
     new_user['password'] = user_data['password']
     return new_user
+
+
+@pytest.fixture(scope="function")
+def token(test_user):
+    return create_access_token({"user_id": test_user["id"]})
+
+
+@pytest.fixture(scope="function")
+def authorized_client(client, token):
+    client.headers = {
+        **client.headers,
+        "Authorization": f"Bearer {token}"
+    }
+    return client
+
+
+@pytest.fixture(scope="function")
+def test_posts(test_user, session):
+    posts_data =[{
+        "title": "first title",
+        "content": "first content",
+        "owner_id": test_user["id"]
+    },
+    {
+        "title": "second title",
+        "content": "second content",
+        "owner_id": test_user["id"]
+    },
+    {
+        "title": "third title",
+        "content": "third content",
+        "owner_id": test_user["id"]
+    }]
+
+    def create_post_model(post):
+        return models.Post(**post)
+
+    post_map = map(create_post_model, posts_data)
+    posts = list(post_map)
+
+    session.add_all(posts)
+    session.commit()
+
+    posts = session.query(models.Post).all()
+
+    return posts
+
---
+++ tests/test_posts.py
@@ -0,0 +1,75 @@
+import pytest
+from app import schemas
+
+
+def test_get_all_posts(authorized_client, test_posts):
+    res = authorized_client.get("/posts/")
+
+    def validate(post):
+        return schemas.PostOut(**post)
+    posts_map = map(validate, res.json())
+    posts_list = list(posts_map)
+
+    assert len(res.json()) == len(test_posts)
+    assert res.status_code == 200
+
+
+def test_unauthorized_user_get_all_posts(client, test_posts):
+    res = client.get("/posts/")
+    assert res.status_code == 401
+
+
+def test_unauthorized_user_get_one_post(client, test_posts):
+    res = client.get(f"/posts/{test_posts[0].id}")
+    assert res.status_code == 401
+
+
+def test_get_one_post_that_not_exists(authorized_client,test_posts):
+    res = authorized_client.get("/posts/999999")
+    assert res.status_code == 404
+
+
+def test_get_one_post(authorized_client, test_posts):
+    res = authorized_client.get(f"/posts/{test_posts[0].id}")
+    print(res.json())
+    post = schemas.PostOut(**res.json())
+    print(post)
+    assert res.status_code == 200
+    assert post.Post.id == test_posts[0].id
+    assert post.Post.content == test_posts[0].content
+    assert post.Post.title == test_posts[0].title
+
+
+@pytest.mark.parametrize("title, content, published", [
+    ("my new title", "my new content", True),
+    ("favorite pizza", "with pineapple", False),
+    ("biggest bread", "chungus", True)
+])
+def test_create_post(authorized_client, test_user, test_posts, title, content, published):
+    res = authorized_client.post("/posts/", json = {"title": title, "content": content, "published": published})
+
+    created_post = schemas.Post(**res.json())
+    assert res.status_code == 201
+    assert created_post.title == title
+    assert created_post.content == content
+    assert created_post.published == published
+    assert created_post.owner_id == test_user['id']
+
+
+def test_create_post_default_published(authorized_client, test_user, test_posts):
+    res = authorized_client.post("/posts/", json = {"title": "my title", "content": "my content"})
+
+    created_post = schemas.Post(**res.json())
+    assert res.status_code == 201
+    assert created_post.title == "my title"
+    assert created_post.content == "my content"
+    assert created_post.published == True
+    assert created_post.owner_id == test_user['id']
+
+
+def test_unauthorized_user_create_post_default_published(client, test_user, test_posts):
+    res = client.post("/posts/", json = {"title": "my title", "content": "my content"})
+
+    assert res.status_code == 401
+
+
